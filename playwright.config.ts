a6116f8d95de817c2256cdff146f713c9c090ad5
Afsal_Veed <gm4afsal@gmail.com>
2024-01-25 11:16:43 +0000

    Modify YAML

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   workers: process.env.CI ? 1 : undefined,
-  reporter: 'html',
+  reporter: process.env.CI ? 'blob' : 'html',
   use: {
      baseURL: 'https://todolist.james.am/#/',
      trace: 'retain-on-failure',
